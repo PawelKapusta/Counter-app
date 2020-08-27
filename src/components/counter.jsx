5d8c760ed477b2b82909c26d513b3321c7e119af
Paweł Kapusta <pawelkapusta70@gmail.com>
2020-08-27 18:46:57 +0200

    modified counter.jsx and add a new file

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
-
+    count: 0,
   };
-
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({count: this.state.count + 1});
+  };
 
   render() {
     return (
         <div>
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button className="btn btn-secondary btn-sm">Increment</button>
+          <button onClick={() => {
+            this.handleIncrement({ id: 1 })
+          }} className="btn btn-secondary btn-sm">Increment</button>
         </div>
     );
   }
